bbd916688aadf3cfe08de2e19c4ec7f1c0f4c077
hoBabu <amansahkumar004@gmail.com>
2023-09-03 17:22:50 +0530

    calculate height of  a tree

---
+++ Daimeter_of_tree.java
@@ -0,0 +1,51 @@
+
+public class Daimeter_of_Tree
+{
+    public static void main(String args[])
+    {
+         Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+        System.out.println(diameter(root).dia);
+/// hey 
+    }
+    static class Node 
+    {
+        int data ;
+        Node left ; 
+        Node right;
+        public Node(int data)
+        {
+            this.data = data ;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    static class Info
+    {
+        int dia;
+        int ht ;
+        public Info(int dia , int ht )
+        {
+            this.dia = dia ;
+            this.ht = ht ;
+        }
+    }
+    public static Info diameter(Node root )
+    {
+        if(root == null )
+        {
+            return new Info(0,0);
+        }
+        Info left = diameter(root.left );
+        Info right = diameter(root.right  );
+
+        int f_diam = Math.max(Math.max(left.dia , right.dia) , left.ht + right.ht+1);
+        int f_height = Math.max(left.ht , right.ht) +1;
+        return new Info(f_diam , f_height );
+    }
+}
